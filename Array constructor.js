56f06138d1f002d08108a17e2532ac35fb005d5c
Kumar Shivam Nair <47927079+Imshivamnair@users.noreply.github.com>
2024-01-24 23:25:08 +0530

    Add files via upload

---
+++ Array constructor.js
@@ -0,0 +1,15 @@
+// Declaration of an empty array 
+// using Array constructor 
+let names = new Array();
+console.log(names);
+
+// Creating and Initializing an array with values 
+let courses = new Array("HTML", "CSS", "Javascript", "React");
+console.log(courses);
+
+// Initializing Array while declaring 
+let arr = new Array(3);
+arr[0] = 10;
+arr[1] = 20;
+arr[2] = 30;
+console.log(arr);
